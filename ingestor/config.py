a7169472af904e6d1e26ec4bd415ad331fe1c2f4
aklatka <arkadiusz.klatka@gmail.com>
2026-05-11 15:46:10 +0200

    Fix: Ingestor didnt work as container

--- ingestor/config.py
+++ ingestor/config.py
@@ -1,9 +1,9 @@
 import os
 
-MQTT_HOST = "127.0.0.1"
+MQTT_HOST = os.getenv("MQTT_HOST", "127.0.0.1")
 MQTT_PORT = 1883
 
 DB_HOST = os.getenv("DB_HOST", "127.0.0.1")
-DB_NAME = "abcd_db"
-DB_USER = "admin"
-DB_PASSWORD = "admin_pass1234"
+DB_USER = os.getenv("DB_USER", "admin")
+DB_PASSWORD = os.getenv("DB_PASS", "admin_pass1234")
+DB_NAME = os.getenv("DB_NAME", "abcd_db")
